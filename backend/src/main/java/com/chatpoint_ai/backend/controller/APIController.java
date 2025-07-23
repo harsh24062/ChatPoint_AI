ee25417a1b11a9e0b5f76c6c9eb052daa7bee7e9
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-07-23 23:27:17 +0530

    APIController done.

---
+++ backend/src/main/java/com/chatpoint_ai/backend/controller/APIController.java
@@ -0,0 +1,38 @@
+package com.chatpoint_ai.backend.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chatpoint_ai.backend.service.QNAService;
+
+@RestController
+@RequestMapping("/api/qna")
+public class APIController {
+
+    @Autowired
+    private QNAService qnaService;
+
+    public ResponseEntity<String> askQuestion(@RequestBody Map<String,String> payload){
+      
+       String question = payload.get("question");
+
+       if(question==null || question.trim().isEmpty()){
+        return new ResponseEntity<>("Question cannot be empty or null",HttpStatus.BAD_REQUEST);
+       }
+
+       try {
+        String answer = qnaService.askQuestion(question);
+        return new ResponseEntity<>(answer,HttpStatus.OK);
+       } catch (Exception e) {
+        return new ResponseEntity<>("Its not your fault, we are facing some issue. Try again later.",HttpStatus.INTERNAL_SERVER_ERROR);
+       }
+       
+    }
+    
+}
---
+++ backend/src/main/java/com/chatpoint_ai/backend/service/QNAService.java
@@ -0,0 +1,11 @@
+package com.chatpoint_ai.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QNAService {
+    
+    public String askQuestion(String question){
+
+    }
+}
