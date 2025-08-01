b8db0acc2fa06510f5d8ab4524fb1d6b437ce827
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-08-01 23:18:23 +0530

    minor fixes

--- backend/src/main/java/com/chatpoint_ai/backend/cors/WebConfig.java
+++ backend/src/main/java/com/chatpoint_ai/backend/cors/WebConfig.java
@@ -3,14 +3,18 @@ package com.chatpoint_ai.backend.cors;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontEnd_URL; 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-              .allowedOrigins("http://localhost:5173")
+              .allowedOrigins(frontEnd_URL)
               .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
               .allowedHeaders("*")
               .allowCredentials(true);
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
  import axios from "axios"
 
- const API_URL = "http://localhost:8080/api/qna/ask"
+ const API_URL = import.meta.env.VITE_API_URL
 
 const fetchChatResponse = async (question) => {
   try {
